54c2445313b3be09ef22f6879522f9e6759c7eb5
Peter Josling <peterjosling@gmail.com>
2014-07-09 09:44:15 +0100

    Correctly display and persist CheckableListItemView state

--- app/list-item-view/list-item-view.js
+++ app/list-item-view/list-item-view.js
@@ -2,12 +2,29 @@ module.exports = Backbone.View.extend({
 	template: require('./list-item-view-template'),
 	className: 'ListItem',
 
+	events: {
+		'change input': 'inputChange'
+	},
+
 	afterRender: function() {
 		this.$el.addClass(this.model.get('class'))
 
 		if (this.model.has('link')) {
 			this.$el.attr('data-uri', this.model.get('link').destination)
 			this.$el.addClass('clickable')
+		}
+
+		if (this.model.get('class') === 'CheckableListItemView') {
+			this.$el.prepend('<input type="checkbox">')
+
+			var checkState = localStorage.getItem('check-' + this.model.id) === 'true'
+			this.$('input')[0].checked = checkState
+		}
+	},
+
+	inputChange: function(e) {
+		if (this.model.get('class') === 'CheckableListItemView' && !this.model.get('volatile')) {
+			localStorage.setItem('check-' + this.model.id, e.currentTarget.checked)
 		}
 	}
 })
