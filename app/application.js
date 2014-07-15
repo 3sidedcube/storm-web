2be7583374d6da2b083d33f162b89e25c36b12d8
Peter Josling <peterjosling@gmail.com>
2014-07-15 11:29:03 +0100

    Support loading app for remote content

--- app/application.js
+++ app/application.js
@@ -13,10 +13,22 @@ module.exports = {
 
 		this.view     = new RootNavigationController()
 
+		if (App.target === App.APP_TARGET_LOCAL) {
+			return App.initLocal()
+		}
+
+		return App.initRemote()
+	},
+
+	initLocal: function() {
 		var appFetch = this.app.fetch(),
 			manifestFetch = this.manifest.fetch()
 
 		return $.when(appFetch, manifestFetch)
+	},
+
+	initRemote: function() {
+		return $.when()
 	},
 
 	setImageDensity: function() {
@@ -31,14 +43,20 @@ module.exports = {
 		}
 	},
 
+	apiRoot: 'https://gdpc.cubeapis.com/v1.5/',
+
 	view: null,
 	app: null,
 	manifest: null,
 	data: {},
 	imageDensity: 1,
 	mode: 0,
+	target: 0,
 	utils: require('utils'),
 
 	APP_MODE_FULL: 0,
-	APP_MODE_PAGE: 1
+	APP_MODE_PAGE: 1,
+
+	APP_TARGET_LOCAL: 0,
+	APP_TARGET_REMOTE: 1
 }
