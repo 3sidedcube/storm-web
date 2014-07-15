08b4bb1d3dcd99146241bd6ac0a3908663eb17e6
Peter Josling <peterjosling@gmail.com>
2014-07-15 08:44:37 +0100

    Extend WP TabbedPageCollection from NavigationController
    Gives it correct methods for constructing child views etc.

--- platforms/wp/app/tabbed-page-collection/tabbed-page-collection-view.js
+++ platforms/wp/app/tabbed-page-collection/tabbed-page-collection-view.js
@@ -1,47 +1,43 @@
-var PageView = require('page-view')
+var NavigationController = require('navigation-controller')
 
-module.exports = PageView.extend({
+module.exports = NavigationController.extend({
 	template: require('./tabbed-page-collection-view-template'),
 	className: 'wp-TabbedPageCollection',
 
-	events: {
-		'click .clickable': 'linkClick'
-	},
-
 	initialize: function() {
-		PageView.prototype.initialize.apply(this, arguments)
+		NavigationController.prototype.initialize.apply(this, arguments)
 
 		this.listViews = []
 
 		var data = [];
-	    for (var i = 0; i < 100; i++) {
-	        data.push({ author: "Adam Smith", titleColor: "rgba(212, 14, 136, 1)", title: "Missed conversation with Michael Brown", previewText: "Michael Brown [1:53 PM]: Thanks for taking the time...", time: "1:55p" });
-	        data.push({ author: "Michael Brown", titleColor: "rgba(212, 14, 136, 1)", title: "Need help later", previewText: "I was hoping you could help me with...", time: "1:50p" });
-	        data.push({ author: "Thomas Lee", titleColor: "rgba(212, 14, 136, 1)", title: "Lunch with you", previewText: "Any chance you want to do lunch on...", time: "1:20p" });
-	        data.push({ author: "Michael Wilson", titleColor: "rgba(212, 14, 136, 1)", title: "QuickStart's and How To's", previewText: "More information on how to use WinJS controls", time: "1:17p" });
-	        data.push({ author: "Gary Paul", titleColor: "rgba(212, 14, 136, 1)", title: "Going out saturday", previewText: "Jean and I are leaving...", time: "12:28p" });
-	        data.push({ author: "Smith Jones", titleColor: "rgba(212, 14, 136, 1)", title: "Happend every time", previewText: "I always end up going to the ...", time: "7:12a" });
-	    }
-	    window.All = { dataSource: new WinJS.Binding.List(data).dataSource };
+		for (var i = 0; i < 100; i++) {
+			data.push({ author: 'Adam Smith', titleColor: 'rgba(212, 14, 136, 1)', title: 'Missed conversation with Michael Brown', previewText: 'Michael Brown [1:53 PM]: Thanks for taking the time...', time: '1:55p' });
+			data.push({ author: 'Michael Brown', titleColor: 'rgba(212, 14, 136, 1)', title: 'Need help later', previewText: 'I was hoping you could help me with...', time: '1:50p' });
+			data.push({ author: 'Thomas Lee', titleColor: 'rgba(212, 14, 136, 1)', title: 'Lunch with you', previewText: 'Any chance you want to do lunch on...', time: '1:20p' });
+			data.push({ author: 'Michael Wilson', titleColor: 'rgba(212, 14, 136, 1)', title: 'QuickStart\'s and How To\'s', previewText: 'More information on how to use WinJS controls', time: '1:17p' });
+			data.push({ author: 'Gary Paul', titleColor: 'rgba(212, 14, 136, 1)', title: 'Going out saturday', previewText: 'Jean and I are leaving...', time: '12:28p' });
+			data.push({ author: 'Smith Jones', titleColor: 'rgba(212, 14, 136, 1)', title: 'Happend every time', previewText: 'I always end up going to the ...', time: '7:12a' });
+		}
+		window.All = { dataSource: new WinJS.Binding.List(data).dataSource };
 
 
-	    data = [];
-	    data.push({ author: "Michael Wilson", titleColor: "rgba(212, 14, 136, 1)", title: "QuickStart's and How To's", previewText: "More information on how to use WinJS controls", time: "1:17p" });
-	    data.push({ author: "Gary Paul", titleColor: "rgba(212, 14, 136, 1)", title: "Going out saturday", previewText: "Jean and I are leaving...", time: "12:28p" });
-	    data.push({ author: "Smith Jones", titleColor: "rgba(212, 14, 136, 1)", title: "Happend every time", previewText: "I always end up going to the ...", time: "7:12a" });
-	    data.push({ author: "Michael Wilson", titleColor: "rgba(212, 14, 136, 1)", title: "QuickStart's and How To's", previewText: "More information on how to use...", time: "1:17p" });
-	    window.Unread = { dataSource: new WinJS.Binding.List(data).dataSource };
+		data = [];
+		data.push({ author: 'Michael Wilson', titleColor: 'rgba(212, 14, 136, 1)', title: 'QuickStart\'s and How To\'s', previewText: 'More information on how to use WinJS controls', time: '1:17p' });
+		data.push({ author: 'Gary Paul', titleColor: 'rgba(212, 14, 136, 1)', title: 'Going out saturday', previewText: 'Jean and I are leaving...', time: '12:28p' });
+		data.push({ author: 'Smith Jones', titleColor: 'rgba(212, 14, 136, 1)', title: 'Happend every time', previewText: 'I always end up going to the ...', time: '7:12a' });
+		data.push({ author: 'Michael Wilson', titleColor: 'rgba(212, 14, 136, 1)', title: 'QuickStart\'s and How To\'s', previewText: 'More information on how to use...', time: '1:17p' });
+		window.Unread = { dataSource: new WinJS.Binding.List(data).dataSource };
 
-	    data = [];
-	    data.push({ author: "Michael Wilson", titleColor: "rgba(212, 14, 136, 1)", title: "QuickStart's and How To's", previewText: "More information on how to use...", time: "1:17p" });
-	    data.push({ author: "Smith Jones", titleColor: "rgba(212, 14, 136, 1)", title: "AppBar and ListView integration", previewText: "Contextual commands in the AppBar", time: "7:12a" });
-	    window.Flagged = { dataSource: new WinJS.Binding.List(data).dataSource };
+		data = [];
+		data.push({ author: 'Michael Wilson', titleColor: 'rgba(212, 14, 136, 1)', title: 'QuickStart\'s and How To\'s', previewText: 'More information on how to use...', time: '1:17p' });
+		data.push({ author: 'Smith Jones', titleColor: 'rgba(212, 14, 136, 1)', title: 'AppBar and ListView integration', previewText: 'Contextual commands in the AppBar', time: '7:12a' });
+		window.Flagged = { dataSource: new WinJS.Binding.List(data).dataSource };
 
-	    data = [];
-	    data.push({ author: "Michael Wilson", titleColor: "rgba(212, 14, 136, 1)", title: "QuickStart's and How To's", previewText: "More information on how to use WinJS controls", time: "1:17p" });
-	    data.push({ author: "Gary Paul", titleColor: "rgba(212, 14, 136, 1)", title: "Going out saturday", previewText: "Jean and I are leaving...", time: "12:28p" });
-	    data.push({ author: "Smith Jones", titleColor: "rgba(212, 14, 136, 1)", title: "Happend every time", previewText: "I always end up going to the ...", time: "7:12a" });
-	    window.Urgent = { dataSource: new WinJS.Binding.List(data).dataSource };
+		data = [];
+		data.push({ author: 'Michael Wilson', titleColor: 'rgba(212, 14, 136, 1)', title: 'QuickStart\'s and How To\'s', previewText: 'More information on how to use WinJS controls', time: '1:17p' });
+		data.push({ author: 'Gary Paul', titleColor: 'rgba(212, 14, 136, 1)', title: 'Going out saturday', previewText: 'Jean and I are leaving...', time: '12:28p' });
+		data.push({ author: 'Smith Jones', titleColor: 'rgba(212, 14, 136, 1)', title: 'Happend every time', previewText: 'I always end up going to the ...', time: '7:12a' });
+		window.Urgent = { dataSource: new WinJS.Binding.List(data).dataSource };
 	},
 
 	getRenderData: function() {
@@ -88,11 +84,5 @@ module.exports = PageView.extend({
 		setTimeout(function() {
 			WinJS.UI.processAll()
 		})
-	},
-
-	linkClick: function(e) {
-		var uri = $(e.currentTarget).data('uri')
-
-		WinJS.Navigation.navigate('test.html')
 	}
 })
