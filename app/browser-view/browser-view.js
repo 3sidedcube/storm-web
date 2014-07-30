9e655edb3aade2d465dfd28b9e2f795578584b60
Peter Josling <peterjosling@gmail.com>
2014-07-30 10:21:57 +0100

    Add browser view

---
+++ app/browser-view/browser-view.js
@@ -0,0 +1,61 @@
+module.exports = Backbone.View.extend({
+	template: require('./browser-view-template'),
+	className: 'browser-view',
+
+	events: {
+		'click .back-button': 'exit',
+		'click .browser-back-button': 'back',
+		'click .browser-forward-button': 'forward',
+		'click .browser-share-button': 'share',
+
+		'touchstart  .browser-footer button': 'buttonTouchStart',
+		'touchend    .browser-footer button': 'buttonTouchEnd',
+		'touchcancel .browser-footer button': 'buttonTouchEnd'
+	},
+
+	afterRender: function() {
+		this.backTarget = null
+		this.iframe = this.$('iframe')[0]
+		this.trigger('ready')
+	},
+
+	// Set iframe location.
+	setUri: function(uri) {
+		this.iframe.src = uri
+	},
+
+	// Store a reference to the previously loaded page so we can navigate back.
+	setBackTarget: function(uri) {
+		this.backTarget = uri
+	},
+
+	// Push iframe back.
+	back: function() {
+		history.back()
+	},
+
+	// Push iframe forward.
+	forward: function() {
+		history.forward()
+	},
+
+	// Open (original) URL in external browser.
+	share: function() {
+		window.open(this.iframe.src, '_blank')
+	},
+
+	// Navigate back to previous page.
+	exit: function() {
+		App.router.navigate(this.backTarget, {trigger: true, replace: true})
+	},
+
+	// Add glow state to buttons on touch.
+	buttonTouchStart: function(e) {
+		e.currentTarget.classList.add('glow')
+	},
+
+	// Remove glow state from buttons.
+	buttonTouchEnd: function(e) {
+		e.currentTarget.classList.remove('glow')
+	}
+})
