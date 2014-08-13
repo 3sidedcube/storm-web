c52c54d2eeb9a060f45c85e1b68b933b8472a2e9
Peter Josling <peterjosling@gmail.com>
2014-08-13 14:55:56 +0100

    Implement CategorySelectionQuestion
    [completed: 9]

--- app/quiz-page/category-selection-question-view.js
+++ app/quiz-page/category-selection-question-view.js
@@ -2,5 +2,53 @@ var QuizQuestion = require('./quiz-question-view')
 
 module.exports = QuizQuestion.extend({
 	template: require('./category-selection-question-view-template'),
-	className: 'CategorySelectionQuestion'
+	className: 'CategorySelectionQuestion',
+
+	afterRender: function() {
+		QuizQuestion.prototype.afterRender.apply(this, arguments)
+
+		var html = '',
+			catCount = this.model.get('categories').length
+
+		this.model.get('options').forEach(function(option, i) {
+			var options = '<td>' + Handlebars.helpers.l(option) + '</td>'
+
+			for (var j = 0; j < catCount; j++) {
+				options += '<td><input type="radio" name="category-' + this.id + '-' + i + '"></td>'
+			}
+
+			html += '<tr>' + options + '</tr>'
+		}, this)
+
+		this.$('.category-grid tbody').html(html)
+	},
+
+	isCorrect: function() {
+		var catCount = this.model.get('categories').length,
+			answer = this.model.get('answer'),
+			selected = []
+
+		for (var option = 0; option < catCount; option++) {
+			selected[option] = -1
+
+			this.$('input[name="category-' + this.id + '-' + option + '"]').each(function(i) {
+				if ($(this).is(':checked')) {
+					selected[option] = i
+				}
+			})
+		}
+
+		// Check array values are the same.
+		if (answer.length !== selected.length) {
+			return false
+		}
+
+		for (var i in answer) {
+			if (answer[i] !== selected[i]) {
+				return false
+			}
+		}
+
+		return true
+	}
 })
