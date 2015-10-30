104732850aae3c0d90e1010d6732c50948b42be4
Peter Josling <peterjosling@gmail.com>
2015-10-30 19:54:09 +0000

    Override page transition style for Windows Phone

--- app/platforms/wp/platform-init.js
+++ app/platforms/wp/platform-init.js
@@ -1,3 +1,25 @@
 require('winjs');
 require('winjs/css/ui-light.css');
 require('./overrides.less');
+
+var NavigationController = require('../../navigation-controller');
+
+var transition = function() {
+  var newPage = this.newPageContent,
+      oldPage = this.pageContent;
+
+  return new Promise(function(resolve) {
+    WinJS.UI.Animation.exitPage(oldPage[0], null).done(function() {
+      newPage.addClass('page-transitioning');
+
+      WinJS.UI.Animation.enterPage(newPage[0], null).done(function() {
+        newPage.removeClass('page-transitioning');
+        resolve();
+      });
+    });
+  });
+};
+
+NavigationController.prototype.transitionForward = transition;
+NavigationController.prototype.transitionBackward = transition;
+NavigationController.prototype.transitionNew = transition;
