f487b16846c9f0a3a2f2d9312eb8d4ea02d7eae8
Peter Josling <peterjosling@gmail.com>
2015-11-06 19:18:43 +0000

    Update RootNavigationController link handling
    Add JavaDoc, factor out methods, handle UriLinks on Windows.

--- app/root-navigation-controller.js
+++ app/root-navigation-controller.js
@@ -16,31 +16,134 @@ module.exports = NavigationController.extend({
   setPageTitle: function() {
   },
 
+  /**
+   * Handles clicks to objects with the 'clickable' class, which contain link
+   * properties as data attributes.
+   *
+   * <p>Possible attributes:
+   * <ul>
+   *   <li>data-uri. The link destination.</li>
+   *   <li>data-link-type. The class of the Storm Link object. If no type is
+   * specified, it is assumed to be InternalLink. SmsLink and EmailLink objects
+   * are not supported and should be reformatted as UriLinks with the correct
+   * protocol.</li>
+   * </ul>
+   *
+   * <p>ExternalLink objects will open a URL in the internal browser (if
+   * possible). UriLink objects will always kick out to the host
+   * browser/system.
+   * @param {Event} e The DOM click event object.
+   */
   linkClick: function(e) {
     var uri      = $(e.currentTarget).data('uri'),
         linkType = $(e.currentTarget).data('link-type');
 
-    if (linkType === 'ExternalLink') {
-      if (!uri) {
-        return;
-      }
+    switch (linkType) {
+      case 'ExternalLink':
+        this.handleExternalLink_(uri);
+        break;
 
-      // Open URL in internal browser.
-      var oldUri = this.currentView.id;
+      case 'UriLink':
+        this.handleUriLink_(uri);
+        break;
 
-      this.setPage('app://browser', false);
-      this.currentView.setUri(uri);
-      this.currentView.setBackTarget(oldUri);
-    } else if (linkType === 'UriLink') {
-      // Open URL in external browser.
-      window.open(uri, '_blank');
-    } else if (linkType === 'ShareLink') {
-      // TODO share functionality
-    } else {
-      this.setPage(uri, false);
-      App.router.navigate(uri);
+      case 'ShareLink':
+        this.handleShareLink_();
+        break;
+
+      case 'TimerLink':
+        this.handleTimerLink_();
+        break;
+
+      case 'AppLink':
+        this.handleAppLink_();
+        break;
+
+      case 'SmsLink':
+      case 'EmailLink':
+        throw new Error('SmsLink and EmailLinks are not supported by the ' +
+            'RootNavigationController. They should be reformatted as UriLink ' +
+            'objects with the correct protocol.');
+
+      case 'InternalLink':
+      default:
+        this.handleInternalLink_(uri);
     }
 
     e.stopPropagation();
+  },
+
+  /**
+   * Handles the URI of an InternalLink, to navigate to another page of the app.
+   * @param {string} uri The URI to navigate to.
+   * @private
+   */
+  handleInternalLink_: function(uri) {
+    this.setPage(uri, false);
+    App.router.navigate(uri);
+  },
+
+  /**
+   * Handles the URI of an ExternalLink object, to be opened in the internal
+   * browser (where possible).
+   * @param {string} uri The URI to navigate to.
+   * @private
+   */
+  handleExternalLink_: function(uri) {
+    if (!uri) {
+      return;
+    }
+
+    // Open URL in internal browser.
+    var oldUri = this.currentView.id;
+
+    this.setPage('app://browser', false);
+    this.currentView.setUri(uri);
+    this.currentView.setBackTarget(oldUri);
+  },
+
+  /**
+   * Handles the URI of a UriLink (i.e. generic handler for multiple
+   * protocols). The URI will be passed to the system/browser for handling as
+   * appropriate. Can also be used with HTTP links to open them in the default
+   * browser instead of the app's embedded browser.
+   * @param {string} uri The URI to navigate to.
+   * @private
+   */
+  handleUriLink_: function(uri) {
+    // Handle Windows apps using the native SDK.
+    if (window.Windows) {
+      var url = new Windows.Foundation.Uri(uri);
+
+      Windows.System.Launcher.launchUriAsync(url);
+      return;
+    }
+
+    // Open URL in a new tab/window.
+    window.open(uri, '_blank');
+  },
+
+  /**
+   * TODO
+   * @private
+   */
+  handleShareLink_: function() {
+    throw new Error('Not yet implemented');
+  },
+
+  /**
+   * TODO
+   * @private
+   */
+  handleTimerLink_: function() {
+    throw new Error('Not yet implemented');
+  },
+
+  /**
+   * TODO
+   * @private
+   */
+  handleAppLink_: function() {
+    throw new Error('Not yet implemented');
   }
 });
