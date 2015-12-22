0e3f3f1424163c2fdd25282e1aae44e6c3ef33a5
Peter Josling <peterjosling@gmail.com>
2015-12-22 08:52:22 +0000

    Support landscape orientations in local video player

--- app/local-video-player-view/local-video-player-view.js
+++ app/local-video-player-view/local-video-player-view.js
@@ -1,3 +1,5 @@
+/* global Windows */
+
 require('./local-video-player-view.less');
 
 module.exports = Backbone.View.extend({
@@ -38,5 +40,52 @@ module.exports = Backbone.View.extend({
   ready: function() {
     this.trigger('ready');
     this.render();
+  },
+
+  /** @override */
+  afterRender: function() {
+    this.unlockDeviceRotation();
+  },
+
+  /** @override */
+  restore: function() {
+    this.unlockDeviceRotation();
+  },
+
+  /** @override */
+  suspend: function() {
+    this.lockDeviceRotation();
+  },
+
+  /**
+   * Unlocks the device orientation (allows portrait or landscape).
+   */
+  unlockDeviceRotation: function() {
+    this.setLandscapeOrientationAllowed(true);
+  },
+
+  /**
+   * Locks the device orientation (allows portrait only).
+   */
+  lockDeviceRotation: function() {
+    this.setLandscapeOrientationAllowed(false);
+  },
+
+  /**
+   * Sets whether or not landscape device orientation should be allowed.
+   * @param {boolean} allowed True if landscape orientation should be allowed.
+   */
+  setLandscapeOrientationAllowed: function(allowed) {
+    if (window.Windows) {
+      var displayInfo  = Windows.Graphics.Display.DisplayInformation,
+          orientations = Windows.Graphics.Display.DisplayOrientations;
+
+      displayInfo.autoRotationPreferences = orientations.portrait;
+
+      if (allowed) {
+        displayInfo.autoRotationPreferences |=
+            orientations.landscape | orientations.landscapeFlipped;
+      }
+    }
   }
 });
